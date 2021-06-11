dc0b2c85cb835d6e5ee0de58eb8b06736d4cc69f
dark2829 <tozcuento.2829@gmail.com>
2021-06-11 01:12:45 -0500

    Modificacion del texto.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,42 +11,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Creado", Toast.LENGTH_SHORT).show();
         // La actividad ha sido creada.
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(this, "OnStart", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Iniciado", Toast.LENGTH_SHORT).show();
         //La actividad esta a punto de hacerse visible
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Toast.makeText(this, "OnResume", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "En ejecución", Toast.LENGTH_SHORT).show();
         // La actividad se ha vuelto visible (ahora se "reanuda").
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Toast.makeText(this, "OnPause", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "En pausa", Toast.LENGTH_SHORT).show();
         // La actividad está en pausa (está a punto de ser detenida).
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Toast.makeText(this, "OnStop", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Detenida", Toast.LENGTH_SHORT).show();
         // La actividad está en pausa, está a punto de cerrarse.
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Toast.makeText(this, "OnDestroy", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Cerrada", Toast.LENGTH_SHORT).show();
         // La actividad se ha cerrado
     }
 }
