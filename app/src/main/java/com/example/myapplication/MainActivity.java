85b735778737e2071c1f4e49a0b32c0fcb60dcbe
dark2829 <tozcuento.2829@gmail.com>
2021-06-12 00:59:51 -0500

    Suma de dos numeros.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,9 +2,16 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText it1;
+    private EditText it2;
+    private TextView ot1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +19,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //Toast.makeText(this, "Creado", Toast.LENGTH_SHORT).show();
         // La actividad ha sido creada.
+
+        it1 = (EditText)findViewById(R.id.itxt_uno);
+        it2 = (EditText)findViewById(R.id.itxt_dos);
+        ot1 = (TextView) findViewById(R.id.otxt_uno);
     }
+
+    public void  Suma (View view){
+        String uno = it1.getText().toString();
+        String dos = it2.getText().toString();
+
+        float num1 = Integer.parseInt(uno);
+        float num2 = Integer.parseInt(dos);
+        float sumaV = num1 + num2;
+
+        String sumaT = String.valueOf(sumaV);
+        ot1.setText(sumaT);
+    }
+
 
     @Override
     protected void onStart() {
